af7460c15aad1aafb65a2e7be01f2650ece12a4a
maryli20032 <maryli.20032@gmail.com>
2021-07-06 22:05:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 if('serviceWorker' in navigator){
+     window.addEventListener('load', function () {
     navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro exitoso de service worker', reg))
     .catch(err => console.warn('Error al tratar de registrar el service worker'))
+     });
 }
