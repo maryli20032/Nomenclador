de5e505f5b2ee75bc852eedf907b1dfb8aaf1fc8
maryli20032 <maryli.20032@gmail.com>
2021-07-06 21:57:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if('serviceWorker' in navigator){
+if('ServiceWorker' in navigator){
     navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro exitoso de service worker', reg))
     .catch(err => console.warn('Error al tratar de registrar el service worker'))
